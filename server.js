283cf08b05a9716c519de301302239e868e96890
LOGANATHANAN <anloganathan20@gmail.com>
2021-05-11 17:42:06 +0530

    updated db

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app=express();
 const http=require('http');
 const socketio=require('socket.io');
 const mongoose=require('mongoose');
-const db=process.env.DB_URL:
+const db=process.env.DB_URL;
 
 const moment=require('moment');
 
